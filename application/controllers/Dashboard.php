19762219ce0b63c318f44d90835820e93fc00458
shiva prasad <shiva@secondsdigital.com>
2021-01-04 15:02:58 +0530

    Dashboard datetime to date

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -59,7 +59,7 @@ class Dashboard extends CI_Controller {
         $data['profile_pic'] = $this->user_model->get_profile_pic_name($data['user_id']);
         $data['profile_pic'] = json_decode( json_encode($data['profile_pic']), true);
         $this->session->set_userdata('profile_pic',$data['profile_pic'][0]['profile_pic']);
-        $data['greeting'] = $this->common_model->getWhere(array("date_added"=>date("Y-m-d")),"todaysgreetings");
+        $data['greeting'] = $this->common_model->getWhere(array("date(date_added)"=>date("Y-m-d")),"todaysgreetings");
 
         if ($this->session->userdata('user_type') == 'user') {
             $data['imp_callbacks'] = $this->callback_model->fetch_important_callbacks($data['user_id']);
